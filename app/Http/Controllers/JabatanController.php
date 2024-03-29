8565a9c905e80094dad288f01739290e74887a9d
Mandri2410 <159509344+Mandri2410@users.noreply.github.com>
2024-03-29 14:39:22 +0700

    update

--- app/Http/Controllers/JabatanController.php
+++ app/Http/Controllers/JabatanController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Jabatan; // Pastikan Anda telah membuat model Jabatan
+use App\Models\Jabatan; 
 
 class JabatanController extends Controller
 {
@@ -14,16 +14,12 @@ class JabatanController extends Controller
 
     public function submitForm(Request $request)
     {
-        // Validasi input jika diperlukan
         $validatedData = $request->validate([
             'nama_jabatan' => 'required|string',
-            // Tambahkan validasi lain jika diperlukan
         ]);
 
-        // Simpan data jabatan ke database
         Jabatan::create($validatedData);
 
-        // Redirect dengan pesan sukses jika diperlukan
         return redirect()->route('form.jabatan')->with('success', 'Data jabatan berhasil disimpan!');
     }
 }
